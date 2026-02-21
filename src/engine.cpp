86039c1116cbcb50850b973eec18ba87501e505e
Rodrigo Delduca <rodrigo@delduca.org>
2026-02-21 16:58:33 -0300

    Work in progress

--- src/engine.cpp
+++ src/engine.cpp
@@ -62,18 +62,7 @@ engine::engine() {
   lua_setfield(L, -2, "width");
   lua_pushinteger(L, height);
   lua_setfield(L, -2, "height");
-
-  lua_newtable(L);
-  lua_pushvalue(L, -2);
-  lua_setfield(L, -2, "__index");
-
-  lua_newtable(L);
-  lua_pushvalue(L, -2);
-  lua_setmetatable(L, -2);
-
   lua_setglobal(L, "viewport");
-
-  lua_pop(L, 2);
 }
 
 void engine::run() {
