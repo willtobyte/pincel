61431e3271f35bb12d4ac4853f890f47b94ab92c
Rodrigo Delduca <rodrigo@delduca.org>
2026-02-27 14:30:11 -0300

    Work in progress

--- cartridge/stages/test.lua
+++ cartridge/stages/test.lua
@@ -3,5 +3,7 @@ return {
     { kind = "player", name = "player", x = 160, y = 90, animation = "idle" },
     { kind = "enemy", name = "dummy", x = 200, y = 90, animation = "idle" },
   },
+  on_enter = function()
+  end,
   on_loop = function(delta) end,
 }
